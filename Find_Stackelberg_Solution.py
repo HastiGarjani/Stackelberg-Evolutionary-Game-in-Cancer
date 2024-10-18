737e18299242f5b83a105551deb431697382a020
HastiGarjani <117920223+HastiGarjani@users.noreply.github.com>
2024-10-18 13:10:35 +0200

    Update Find_Stackelberg_Solution.py
    Recalculating the equilibrium after projection

--- Find_Stackelberg_Solution.py
+++ Find_Stackelberg_Solution.py
@@ -1,5 +1,5 @@
 ################################################################
-####################  Stackelberg Strategy  ####################
+############# Code to Obtain Stackelberg Strategy ##############
 
 import numpy as np
 
@@ -8,7 +8,7 @@ K_max = 10000
 r_max = 0.45
 T = 0.7*K_max
 d = 0.01
-g = 1
+g = 0.8
 sigma = 1
 theta = 0.1
 k = 2
@@ -18,9 +18,7 @@ m_eff = 0.5
 aRS = 0.9
 aSR = 0.15
 # Quality of life coefficients
-# a1 = 0.54
-# a2 = 0
-# a3 = 0.25
+
 a1 = 0.5
 a2 = 0.25
 a3 = 0.25
@@ -35,7 +33,9 @@ xR = (K_max/(r_max*(1-aSR*aRS)))*(-m*np.exp(g*uR)/(k+b*uR) + aRS*m/k - d*np.exp(
     + r_max - aRS*r_max + aRS*d)
 xS = (K_max/(r_max*(1-aSR*aRS)))*(aSR*m*np.exp(g*uR)/(k+b*uR) - m/k + aSR*d*np.exp(g*uR) \
     + r_max -d - aSR*r_max)
+xR_dominant = K_max*(1-((d+m/(k+b*uR))*np.exp(g*uR))/r_max)
 xS[xS<0] = 0
+xR[xS<0] = xR_dominant[xS<0]
 # Quality of life function defined over stabilization region
 Q = 1 - a1*(((xR+xS)/K_max)**2) - a2*(uR**2) - a3*(m**2)
 Q[np.add(xS,xR)>7000] = 0
